9972764b8367bf4bf216ed128bd8dc440e8c6f23
MelvinChoguaj <143530814+MelvinChoguaj@users.noreply.github.com>
2023-09-07 07:45:44 -0600

    TS5_MACL_1105723

---
+++ TS5_MACL_1105723.py
@@ -0,0 +1,29 @@
+'''Introduccion a la programacion supervisado
+7/09/2023
+Melvin Choguaj
+Aprender a utilizar las variables y a imprimir en pantalla'''
+
+print("Mi segundo programa")
+
+print("ingrese su nombre:")
+sNombre= input()
+
+print("ingrese su edad:")
+sEdad= input()
+
+print("ingrese su carrera:")
+sCarrera= input()
+
+print("ingrese su Carne:")
+sCarne= input()
+
+print("Mi segundo programa")
+print("Nombre:"+ sNombre)
+print("Edad:"+ sEdad)
+print("Carrera:"+ sCarrera)
+print("Carne:"+ sCarne)
+
+print("Soy", sNombre, end="")
+print(", tengo", sEdad, end="")
+print(" años y estudio la carrera de", sCarrera, end="")
+print(". Mi numero de carné es", sCarne, end="")
